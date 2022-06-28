7254b281f541ac39fabea6692193b655f5503c13
Alena Ziaziulchyk <alen4ik2743192@gmail.com>
2022-06-28 16:43:19 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const str = String(n);
+    const arr = str.split("");
+    const reversedArr = arr.reverse();
+    const newStr = reversedArr.join("");
+
+    return parseInt(newStr);
 }
